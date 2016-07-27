b2ed03d0587a3d2513f747640108e8ad85ae0e3c
Arsen Melikyan <bugron@mail.ru>
2016-07-28 01:57:52 +0400

    Fix error '413 Request Entity Too Large'

--- app.js
+++ app.js
@@ -14,8 +14,12 @@ app.set('view engine', 'pug');
 
 app.use(favicon(path.join(__dirname, '/public/favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({
+  parameterLimit: 10000000,
+  limit: '50mb',
+  extended: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Make our db accessible to our router
